79c9f1f1b27103821ab970b644e7d1e187c291c0
Saul Castro <saulcastroies@gmail.com>
2025-01-13 01:13:19 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", () => {
-  const scrollValue = window.scrollY; // Obtener la cantidad de scroll
-  const blueIntensity = Math.min(255, scrollValue); // Limitar el máximo
-  document.body.style.backgroundColor = `rgb(0, ${50 + blueIntensity / 10}, ${blueIntensity})`;
-});
-
